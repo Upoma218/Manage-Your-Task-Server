6088a06509e39621d6140373e5b2aca03e65f855
Upoma21801~ <upoma.cdsr@gmail.com>
2022-12-29 04:44:53 +0600

    task api added

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.4cizlao.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
 
@@ -28,11 +34,32 @@ function verifyJWT(req, res, next){
 }
 async function run(){
     try{
+        const tasksCollection = client.db('manageYourTasks').collection('tasks');
+
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1d'});
             res.send({token});
         })
+        // Tasks get api
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const task = await tasksCollection.find(query).toArray();
+            res.send(task);
+        })
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await tasksCollection.findOne(filter);
+            res.send(result);
+        })
+        // Tasks post api
+        app.post('/tasks',async (req, res) => {
+            const tasks = req.body;
+            const result = await tasksCollection.insertOne(tasks);
+            res.send(result);
+
+        });
     }
     finally{
 
